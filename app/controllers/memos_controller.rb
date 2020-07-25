25b2b3f32afaed550a0819ceee113e6a66cb2902
Kota Tsunodae <tsunoda.0688k@outlook.jp>
2020-07-25 12:19:07 +0900

    initail-commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,13 @@
+class MemosController < ApplicationController
+    def index
+    #トップページにアクセスされた時のアクション
+    end
+
+    def new
+    #新規作成ページにアクセスされた時のアクション
+    end
+    
+    def create
+    #メモが新規作成ページからpostされた時のアクション
+    end
+end
